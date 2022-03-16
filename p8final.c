0b44029cc85e9ac656838e8e77497e5b60e29472
Dhaarini Hathwar <96413511+Dhaarini24@users.noreply.github.com>
2022-03-16 13:41:05 +0000

    p7

--- p8final.c
+++ p8final.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+struct _fraction
+{
+int num,den;
+};
+typedef _fraction Fraction;
+int find_gcd(int a, int b)
+{
+  int t;
+    while (b!=0)
+      {
+        t = b;
+        b = a%b;
+        a = t;
+      }
+    return a;
+}
+Fraction input_fraction()
+{
+  
+}
